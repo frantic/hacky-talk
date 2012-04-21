9f66c5900d8574763106328a7d46a2670c0f7f0a
Frantic <alex.frantic@gmail.com>
2012-04-21 17:40:40 +0300

    Initial Commit

---
+++ HackyTalk/HTAppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  HTAppDelegate.h
+//  HackyTalk
+//
+//  Created by Frantic on 4/21/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class HTViewController;
+
+@interface HTAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) HTViewController *viewController;
+
+@end
---
+++ HackyTalk/HTViewController.h
@@ -0,0 +1,13 @@
+//
+//  HTViewController.h
+//  HackyTalk
+//
+//  Created by Frantic on 4/21/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HTViewController : UIViewController
+
+@end
